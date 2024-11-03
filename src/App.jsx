fd6ff2847bebddd84b830e14011e451ec7eb16dc
zeroban <theemptyspamfolder@gmail.com>
2024-11-03 10:30:45 -0500

    Created To-do list with ul elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Created an array of objects  
+const todoList = [
+  {
+    id:1,
+    title: "Complete assignment 1"
+  },
+  {
+    id:2,
+    title: "Complete assignment 2"
+  },
+  {
+    id:3,
+    title: "Complete assignment 3"
+  }
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>To-Do List</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
 export default App
